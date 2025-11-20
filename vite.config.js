d387c85aea9b709f39da311cb3b7b23f4d6a463d
DevJustice161 <webdevjustice@gmail.com>
2025-11-20 17:52:20 +0100

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
+        // eslint-disable-next-line no-undef
+        base: process.env.VITE_BASE_PATH || "/dolley-s-laundry",
       },
     }),
   ],
-})
+});
